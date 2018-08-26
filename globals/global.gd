68fab2dabf6b387b106be2df4a3c6e757b59020c
salvob <tholalak@yahoo.it>
2018-08-26 18:49:33 +0200

    added persistance

--- globals/global.gd
+++ globals/global.gd
@@ -1,1 +1,16 @@
 # global script
+
+func _ready():
+	# if we want to save data from global
+	add_to_group("persist")
+	pass
+
+func get_state():
+	var save_dict = {
+		# whatever we want to save
+	}
+	return save_dict
+
+func load_state(data):
+	for attribute in data:
+		set(attribute, data[attribute])
