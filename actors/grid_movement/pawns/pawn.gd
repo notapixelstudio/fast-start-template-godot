fe61d3e00fe0231770bef31d06a510584404937d
salvob <tholalak@yahoo.it>
2019-01-10 17:19:59 +0100

    big refactoring and delete useless, obsolete file. Rename many others

--- actors/grid_movement/pawns/pawn.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-enum CELL_TYPES { ACTOR, OBSTACLE, OBJECT }
-export(CELL_TYPES) var type = ACTOR
---
+++ actors/platformer/states/Die.gd
@@ -0,0 +1,9 @@
+extends State
+
+# Initialize the state. E.g. change the animation
+func enter():
+	owner.set_dead(true)
+	owner.get_node("AnimationPlayer").play("die")
+
+func _on_animation_finished(anim_name):
+	emit_signal("finished", "dead")
